bea6abf9245c4dc953a84b16ab1355affbdb16cf
Ayad <ayadlawrani65@gmail.com>
2024-01-10 22:09:53 +0300

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import NavBar from "./Components/NavBar.tsx";
+import HomePage from "./Components/homePage/HomePage.tsx";
+import Footer from "./Components/Footer.tsx";
+
 function App() {
 
   return (
     <>
     <NavBar/>
+      <HomePage/>
+      <Footer/>
     </>
   )
 }
