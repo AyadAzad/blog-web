674d340eb4e5294107857ad6e3aeed45ec4b2fa3
arybinskaya <aleksalaugh@gmail.com>
2024-01-11 17:51:28 +0300

    Home page is done

--- src/Components/homePage/HomePage.tsx
+++ src/Components/homePage/HomePage.tsx
@@ -2,6 +2,10 @@ import menWithComputer from "../../assets/menWithComputer.png";
 import BusinessCard from "./BusinessCard";
 import ChooseCategory from "./ChooseCategory";
 import FuturePosts from "./FuturePosts";
+import JoinNow from "./JoinNow";
+import ListOfAuthers from "./ListOfAuthers";
+import Testimonials from "./Testimonials";
+import WhyWeStarted from "./WhyWeStarted";
 
 const HomePage = () => {
   return (
@@ -9,7 +13,7 @@ const HomePage = () => {
       <div className="w-full relative">
         <img className="w-full brightness-50" src={menWithComputer} />
         <div className="absolute top-52 left-20 w-3/4 h-96 text-white font-sans ">
-          <p className="top-52 text-base pb-11 ">
+          <p className="top-52 text-base pb-11">
             POSTED ON <b>STARTUP</b>
           </p>
           <p className="pb-11 text-6xl w-3/4">
@@ -32,6 +36,10 @@ const HomePage = () => {
       <FuturePosts />
       <BusinessCard />
       <ChooseCategory />
+      <WhyWeStarted />
+      <ListOfAuthers />
+      <Testimonials />
+      <JoinNow />
     </>
   );
 };
---
+++ src/Components/homePage/JoinNow.tsx
@@ -0,0 +1,22 @@
+const JoinNow = () => {
+  return (
+    <div className="w-full  flex justify-center items-center">
+      <div className="w-3/12 ">
+        <div className="w-full pt-10 text-center pb-20">
+          <p className="pb-11 font-bold text-3xl text-center pt-20">
+            Join our team to be a part of our story
+          </p>
+          <p className="text-slate-500 pb-11">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt.
+          </p>
+          <button className=" font-bold pb-2 bg-amber-300 text-black w-52 h-14">
+            Join Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JoinNow;
