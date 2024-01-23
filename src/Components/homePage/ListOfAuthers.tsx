8b7be7734232f75af22b08b7f3b24dfd080427e1
arybinskaya <aleksalaugh@gmail.com>
2024-01-23 15:16:35 +0300

    Chanded Authers

--- src/Components/homePage/ListOfAuthers.tsx
+++ src/Components/homePage/ListOfAuthers.tsx
@@ -1,5 +1,5 @@
-import man1 from "../../assets/man1.png";
-import women1 from "../../assets/women1.png";
+import Ayad_Azad from "../../assets/Ayad_Azad.png";
+import Alexandra_Rybinskaya from "../../assets/Alexandra_Rybinskaya.jpg";
 import women2 from "../../assets/women2.png";
 import women3 from "../../assets/women3.png";
 import socialWrapper from "../../assets/SocialWrapper.png";
@@ -14,12 +14,10 @@ const ListOfAuthers = () => {
       <div className="flex flex-col md:flex-row pb-10 md:pb-20 w-full md:w-10/12 mx-auto">
         <div className="bg-slate-200 mx-auto my-5 md:my-0 md:mx-5 px-10 pt-10 hover:bg-amber-100 md:w-72">
           <div className="flex items-center justify-center">
-            <img src={man1} alt="Floyd Miles"></img>
+            <img className="w-32 h-32" src={Ayad_Azad} alt="Ayad Azad"></img>
           </div>
           <p className="pb-3 md:pb-5 font-bold text-lg md:text-2xl text-center">
-            <a href="/authors">
-            Floyd Miles
-            </a>
+            <a href="/authors">Ayad Azad</a>
           </p>
           <p className="text-slate-500 pb-6 md:pb-11 text-center">
             Content Writer @Company
@@ -29,12 +27,16 @@ const ListOfAuthers = () => {
           </div>
         </div>
 
-        <div className="bg-slate-200 mx-auto my-5 md:my-0 md:mx-5 px-10 pt-10 hover:bg-amber-100 md:w-72">
+        <div className="bg-slate-200 mx-auto my-5 md:my-0 md:mx-5 md:px-0 px-10 pt-10 hover:bg-amber-100 md:w-72">
           <div className="flex items-center justify-center">
-            <img src={women1} alt="Dianne Russell"></img>
+            <img
+              className=" rounded-full w-32 h-32"
+              src={Alexandra_Rybinskaya}
+              alt="Alexandra Rybinskaya"
+            ></img>
           </div>
           <p className="pb-3 md:pb-5 font-bold text-lg md:text-2xl text-center">
-            Dianne Russell
+            Alexandra Rybinskaya
           </p>
           <p className="text-slate-500 pb-6 md:pb-11 text-center">
             Content Writer @Company
